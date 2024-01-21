fc10cbfa73f19bc0e5366377f650a5d6e39165ec
João Rodrigues <jprocharodrigues2003@gmail.com>
2024-01-21 10:24:57 +0000

    auction page

---
+++ LIForCars/wwwroot/js/auction.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const bidButton = document.querySelector('.bidbutton');
+    const buyButton = document.querySelector('.buybutton');
+    
+    const bidPage = document.getElementById('bid-page');
+    const buyPage = document.getElementById('buy-page');
+
+    bidButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        showBidPage();
+    });
+
+    buyButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        showBuyPage();
+    });
+
+
+});
+
+
+function showBidPage() {
+    document.getElementById('bid-page').style.display = 'block';
+}
+
+function closeBidPage() {
+    document.getElementById('bid-page').style.display = 'none';
+}
+
+function showBuyPage() {
+    document.getElementById('buy-page').style.display = 'block';
+}
+
+function closeBuyPage() {
+    document.getElementById('buy-page').style.display = 'none';
+}
