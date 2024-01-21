839b4ea13e257d0034b8db9cec569b93c0cc4a06
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-01-21 19:40:07 +0000

    Add Auction page and update auction.css

---
+++ LIForCars/Pages/Auction.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class AuctionModel : PageModel
+{
+    public IActionResult OnGetHeaderPartial() => Partial("Shared/Header");
+    public IActionResult OnGetFooterPartial() => Partial("Shared/Footer");
+    public IActionResult OnGetAboutPartial() => Partial("Shared/About");
+    public IActionResult OnGetContactPartial() => Partial("Shared/Contact");
+    public IActionResult OnGetLoginPartial() => Partial("Shared/Login");
+    public IActionResult OnGetRegisterPartial() => Partial("Shared/Register");
+}
