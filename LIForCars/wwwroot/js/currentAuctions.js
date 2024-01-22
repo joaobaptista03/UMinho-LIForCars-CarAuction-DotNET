0de62e5a5110b8dbc15cdfd091bf1cb94690d320
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-01-22 03:02:07 +0000

    Add view auction button and update pagination text color

--- LIForCars/wwwroot/js/currentAuctions.js
+++ LIForCars/wwwroot/js/currentAuctions.js
@@ -1,3 +1,7 @@
 function navigateToPage(page) {
     window.location.href = '/CurrentAuctions?CurrentPage=' + page + '&PageSize=@Model.PageSize';
 }
+
+function navigateToAuction(auctionId) {
+    window.location.href = '/Auction/?id=' + auctionId;
+}
